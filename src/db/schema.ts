ee87dacd200f7b2ce701aa195bb37989d74ef27e
Dwi Andhika <dwiandhikaaji@gmail.com>
2023-12-02 23:49:05 +0700

    adjust schema attachment

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -60,7 +60,9 @@ export const user_submission_answer = pgTable('user_submission_answer', {
 
 export const attachment = pgTable('attachment', {
     id: serial('id').primaryKey(),
+    uploader_nik: text('uploader_nik').references(() => user.nik),
     mime_type: text('mime_type'),
+    real_filename: text('real_filename'),
     path: text('path'),
     data: jsonb('data'),
     created_at: timestamp('created_at').defaultNow(),
