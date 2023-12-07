2dd748c42a47c514ef176c2c02b7a20a1431b9d6
itsweape <rengganis.wahyuaning.putri@mail.ugm.ac.id>
2023-12-07 19:21:18 +0700

    event, periode, and approval bansos API

---
+++ src/routes/bansos/event/get.ts
@@ -0,0 +1,65 @@
+import { FastifyInstance } from "fastify";
+import * as d from "drizzle-orm";
+import { sql } from "drizzle-orm";
+import { db } from "@db/database";
+import { bansos_event } from "@db/schema";
+
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+// Request and Response schema
+const requestSchema = z.object({
+    bansos_provider_id: z.coerce.number().optional(),
+});
+
+const responseSchema = z.object({
+    message: z.string().default("success").optional(),
+    result: z.array(z.string()),
+});
+
+export default async function route(fastify: FastifyInstance) {
+    fastify.withTypeProvider<ZodTypeProvider>().route({
+        method: "GET",
+        url: "/bansos/event",
+        onRequest: [fastify.authenticate],
+        schema: {
+            tags: ["bansos"],
+            description: "get periode bansos",
+            querystring: requestSchema,
+            response: {
+                "2xx": responseSchema,
+            },
+            security: [
+                {
+                    Bearer: [],
+                },
+            ],
+        },
+        handler: async (request, reply) => {
+            if (request.user.role !== "admin") {
+                return reply.forbidden("Access denied");
+            }
+
+            let periode = db
+                .select({
+                    periode: sql<string>`to_char(${bansos_event.start_date}, 'Mon YYYY')`,
+                })
+                .from(bansos_event)
+                .orderBy(sql`start_date DESC`)
+                .$dynamic();
+
+            if (request.query.bansos_provider_id) {
+                periode = periode
+                .where(d.eq(bansos_event.bansos_provider_id, request.query.bansos_provider_id));
+            }
+
+            const list = await periode;
+            const list_periode = list.map((item) => item.periode);
+
+            return {
+                message: "success",
+                result: list_periode,
+            };
+        },
+    });
+}
---
+++ src/routes/submission/put.ts
@@ -0,0 +1,83 @@
+import { FastifyInstance } from "fastify";
+
+import { sql } from "drizzle-orm";
+import * as d from "drizzle-orm";
+import { db } from "@db/database";
+import { bansos_event, user_submission } from "@db/schema";
+
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+
+// Request and Response schema
+const requestSchema = z.object({
+    user_submission_id: z.coerce.number(),
+    status: z.enum(["accept", "reject"]),
+});
+const responseSchema = z.object({
+    message: z.string().default("success").optional(),
+    result: z.object({
+        nik: z.string().nullish(),
+        bansos_event: z.string().nullish(),
+        status: z.enum(["accept", "reject"]),
+    }),
+});
+
+export default async function route(fastify: FastifyInstance) {
+    fastify.withTypeProvider<ZodTypeProvider>().route({
+        method: "PUT",
+        url: "/submission",
+        onRequest: [fastify.authenticate],
+        schema: {
+            tags: ["submission"],
+            description: "put status submission",
+            querystring: requestSchema,
+            response: {
+                "2xx": responseSchema,
+            },
+            security: [
+                {
+                    Bearer: [],
+                },
+            ],
+        },
+        handler: async (request, reply) => {
+            const user_submission_id = request.query.user_submission_id;
+            const status = request.query.status;
+            
+            if (request.user.role !== "admin") {
+                return reply.forbidden("Access denied")
+            }
+
+            const user_approval = await db
+                .select({
+                    nik: user_submission.nik,
+                    status: user_submission.status,
+                    bansos_event: bansos_event.name,
+                })
+                .from(user_submission)
+                .where(d.eq(user_submission.id, user_submission_id))
+                .innerJoin(bansos_event, d.eq(user_submission.bansos_event_id, bansos_event.id));
+            
+            if (user_approval.length === 0) {
+                    reply.notFound("id user_submission not found");
+                    return;
+                }
+
+            await db
+                .update(user_submission)
+                .set({
+                    status: status,
+                })
+                .where(d.eq(user_submission.id, user_submission_id));
+
+            return {
+                message: "succes",
+                result: {
+                    nik: user_approval[0].nik,
+                    bansos_event: user_approval[0].bansos_event,
+                    status: status ?? user_approval[0].status,
+                },
+            };
+        },
+    });
+}
