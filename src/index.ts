ddb8b2120324e0ef7f4385fd50388b648b30fc46
Dwi Andhika <dwiandhikaaji@gmail.com>
2023-12-18 14:20:25 +0700

    deploy 0.3.3

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ async function main() {
             info: {
                 title: "Capstone REST API",
                 description: "Capstone Project API",
-                version: "0.3.2",
+                version: "0.3.3",
             },
             servers: [],
             components: {
